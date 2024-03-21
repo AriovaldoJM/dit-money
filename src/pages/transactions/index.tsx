a3121c7498a09d4930fba8f2e43dce225743304b
Ariovaldo Moraes <88855698+AriovaldoJM@users.noreply.github.com>
2024-03-21 11:07:09 -0300

    Atulazação de arquivos

---
+++ src/pages/transactions/index.tsx
@@ -0,0 +1,11 @@
+import { Header } from "../../components/Header";
+import { Summary } from "../../components/Header/Summry";
+
+export function Transactions(){
+    return (
+        <div>
+            <Header />
+            <Summary />
+        </div>
+    )
+}
